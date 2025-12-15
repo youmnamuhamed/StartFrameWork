26317d2d937880acae2e96576e3b9f3eae8c78c2
Youmna Muhammed <youmnamuhammed21@gmail.com>
2025-12-15 05:14:42 +0300

    update 3

--- firstreactproject/src/main.jsx
+++ firstreactproject/src/main.jsx
@@ -10,6 +10,9 @@ import '@fortawesome/fontawesome-free/css/all.min.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/StartFrameWork">
+      <App />
+    </BrowserRouter>
+ 
   </StrictMode>,
 )
