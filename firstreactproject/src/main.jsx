b73ae896399def010ed29b317ca32130f3da1de6
Youmna Muhammed <youmnamuhammed21@gmail.com>
2025-12-15 04:22:07 +0300

    update 6

--- firstreactproject/src/main.jsx
+++ firstreactproject/src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import { BrowserRouter } from 'react-router-dom';
+
 
 
 
